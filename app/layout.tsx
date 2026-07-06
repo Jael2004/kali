8da3941d9d3b2492ee67a13c626614a78d30ad18
Jael2004 <jaeleben08@gmail.com>
2026-07-06 16:57:11 +0000

    Create RootLayout component with metadata

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const metadata = {
+  title: "Pour mon bébé Kali💜",
+  description: "Joyeux Anniversaire Babe !",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
